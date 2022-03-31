fd31cb2d8ec6809c1f84b12123d02c72ce733feb
Maksim <runrostongamer@gmail.com>
2022-03-31 19:11:06 +0300

    slick mobile complete

--- src/js/script.js
+++ src/js/script.js
@@ -5,11 +5,36 @@ $(document).ready(function() {
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [{
-            breakpoint: 992,
-            settings: {
-                dots: true,
-                arrows: false
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    dots: true,
+
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    dots: true,
+
+                }
             }
-        }]
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
